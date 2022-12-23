1fdb80b7fdb961ad7017115179b32e0c10962db7
tknickman <tom.knickman@vercel.com>
2022-12-23 10:51:52 -0500

    test with ffi

--- crates/turbo-mock/src/main.rs
+++ crates/turbo-mock/src/main.rs
@@ -1,3 +1,12 @@
+mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+use crate::ffi::{nativeRunWithArgs, GoString};
+use std::{ffi::CString, process};
+
+// This function should not expanded. Please add any logic to
+// `turborepo_lib::main` instead
 fn main() {
     println!("Hello, world!");
 }
